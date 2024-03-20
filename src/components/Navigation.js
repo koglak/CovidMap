e8649b0a9f0f17ca9e70de6130f7dd0acbd07f79
kardelenoglak <koglakci@ford.com.tr>
2024-03-20 21:00:50 +0300

    branding is added to nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,10 @@ function Navigation() {
     return (
         <Navbar bg="white" expand="lg" className="shadow-sm ">
             <Container fluid>
-                <Navbar.Brand href="/">Covid Map</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={require("../images/branding.png")} width="35" height="35" style={{ cursor: "pointer", margin:"5px" }} alt="branding" />
+                    <label style={{ cursor: "pointer", fontWeight: "650" }}>Covid Map</label>
+                </Navbar.Brand>
             </Container>
         </Navbar>
     );
